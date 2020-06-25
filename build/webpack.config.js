686ba446d124d3d77e8d2f2e969c332b45c4e183
Dan Stauffer <stauffer6638@gmail.com>
2020-06-25 00:24:30 -0400

    basic webpack setup

---
+++ build/webpack.config.js
@@ -0,0 +1,82 @@
+const { join, resolve, path } = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+const isLocal = process.env.NODE_ENV === 'local';
+const rootDir = join(__dirname, '..');
+const { version } = require('../package.json');
+
+// Configuration options used by webpack build code
+const config = {
+    isProd,
+    isDev,
+    isLocal,
+    rootDir,
+    version,
+};
+
+module.exports = (env = {}) => ({
+    mode: env.prod ? 'production' : 'development',
+    devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+    entry: {
+        app: './src/modules/core/js/create-app.js',
+    },
+    output: {
+        //path: resolve(config.rootDir, 'dist'),
+        path: join(config.rootDir, 'dist/'),
+        publicPath: '/dist/',
+        filename: isProd ? '[name].[chunkhash].js' : '[name].js',
+        chunkFilename: isProd ? '_[name].[chunkhash].js' : '_[name].js',
+    },
+    resolve: {
+        alias: {
+            // this isn't technically needed, since the default `vue` entry for bundlers
+            // is a simple `export * from '@vue/runtime-dom`. However having this
+            // extra re-export somehow causes webpack to always invalidate the module
+            // on the first HMR update and causes the page to reload.
+            'vue': '@vue/runtime-dom',
+            '~modules': resolve(config.rootDir, 'src/modules'),
+            '~routes': resolve(config.rootDir, 'src/routes')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.png$/,
+                use: {
+                  loader: 'url-loader',
+                  options: { limit: 8192 }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { hmr: !env.prod }
+                    },
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        inline: true,
+        hot: true,
+        stats: 'minimal',
+        contentBase: rootDir,
+        overlay: true
+    }
+});
