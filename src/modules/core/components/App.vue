cff752dcacffee0d97652cea45a37203bf3e48b0
Dan Stauffer <stauffer6638@gmail.com>
2020-06-25 00:24:38 -0400

    simple working code

---
+++ src/modules/core/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="toggleModalState">Open modal</button>
+    <modal v-if="modalOpen">
+        <p>Hello, I'm a modal window.</p>
+    </modal>
+</template>
+
+<script>
+
+import Modal from '~modules/core/components/Modal.vue';
+
+export default {
+    components: {
+        Modal
+    },
+    data: () => ({
+        modalOpen: true
+    }),
+    methods: {
+        toggleModalState() {
+            this.modalOpen = !this.modalOpen;
+        }
+    }
+};
+</script>
