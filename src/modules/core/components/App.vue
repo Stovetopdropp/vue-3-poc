ac3e2f6e57208bc517eb6b6a1dc72036ec3e7171
Dan Stauffer <stauffer6638@gmail.com>
2020-06-25 00:42:53 -0400

    emits definition

--- src/modules/core/components/App.vue
+++ src/modules/core/components/App.vue
@@ -1,7 +1,10 @@
 <template>
     <button @click="toggleModalState">Open modal</button>
     <teleport to="#modal-wrapper">
-        <modal v-if="modalOpen">
+        <modal
+            v-if="modalOpen"
+            @close="toggleModalState"
+        >
             <p>Hello, I'm a modal window. teleport massive</p>
         </modal>
     </teleport>
@@ -14,6 +17,7 @@ import { ref } from 'vue';
 import Modal from '~modules/core/components/Modal.vue';
 
 export default {
+    name: 'App',
     components: {
         Modal
     },
--- src/modules/core/components/Modal.vue
+++ src/modules/core/components/Modal.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="modal">
         <slot></slot>
+        <button @click="$emit('close')">Dismiss</button>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Modal',
+    emits: ['close']
+}
+</script>
