9b19493ad8cb08d8f86e994741b03458d79a6e4e
Dan Stauffer <stauffer6638@gmail.com>
2020-06-25 00:47:23 -0400

    Styling slot content

--- src/modules/core/components/Modal.vue
+++ src/modules/core/components/Modal.vue
@@ -6,8 +6,17 @@
 </template>
 
 <script>
+
 export default {
     name: 'Modal',
     emits: ['close']
 }
+
 </script>
+
+<style scoped>
+    /* allows you to target slot content with scoped rules in the component providing the slot */
+    ::v-slotted(p) {
+        font-style: italic;
+    }
+</style>
