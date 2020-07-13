fbf6cf8b59b19021f65ca31c4f01bdd9e50e7b71
Dan Stauffer <stauffer6638@gmail.com>
2020-07-13 13:54:38 -0400

    form element

---
+++ src/modules/core/components/ui/form-elements/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="c-base-input">
+        <div
+            class="c-base-input__container"
+        >
+            <input
+                :value="modelValue"
+                @input="$emit(
+                    'update:modelValue',
+                    $event.target.value
+                )"
+                class="c-base-input__input"
+              >
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BaseInput',
+    props: {
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        }
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
--- src/routes/homepage/components/HomepageContainer.vue
+++ src/routes/homepage/components/HomepageContainer.vue
@@ -1,6 +1,15 @@
 <template>
     <Layout>
         <h1>Its the homepage yall</h1>
+        <!--
+            v-model="homepageInputValue"
+            is shorthand for
+            v-model:modelValue="homepageInputValue"
+        -->
+        <h3>Single v-model binding</h3>
+        <BaseInput
+            v-model="homepageInputValue"
+        />
     </Layout>
 </template>
 
@@ -8,11 +17,19 @@
 
 import Layout from '~modules/core/components/layouts/Layout.vue';
 
+import BaseInput from '~modules/core/components/ui/form-elements/BaseInput.vue';
+
 export default {
     name: 'HomepageContainer',
     components: {
         Layout,
+        BaseInput
     },
+    data() {
+        return {
+            homepageInputValue: '',
+        }
+    }
 };
 
 </script>
